365ef0c727160f4e62a3a16413fdf6dbc71a7f1f
Roselle <rosellemilvich@65.126.174.66>
2014-02-17 14:46:13 -0700

    x

--- Project/gen/com/example/inventoryhombres/R.java
+++ Project/gen/com/example/inventoryhombres/R.java
@@ -17,41 +17,41 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static int activity_horizontal_margin=0x7f040000;
-        public static int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static int ic_launcher=0x7f020000;
+        public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080000;
     }
     public static final class layout {
-        public static int activity_main=0x7f030000;
-        public static int activity_table_populate=0x7f030001;
-        public static int activity_table_select=0x7f030002;
+        public static final int activity_main=0x7f030000;
+        public static final int activity_table_populate=0x7f030001;
+        public static final int activity_table_select=0x7f030002;
     }
     public static final class menu {
-        public static int main=0x7f070000;
-        public static int table_populate=0x7f070001;
-        public static int table_select=0x7f070002;
+        public static final int main=0x7f070000;
+        public static final int table_populate=0x7f070001;
+        public static final int table_select=0x7f070002;
     }
     public static final class string {
-        public static int action_search=0x7f050007;
-        public static int action_settings=0x7f050003;
-        public static int app_name=0x7f050000;
-        public static int big_booties=0x7f05000c;
-        public static int button_edit=0x7f05000b;
-        public static int button_inventory=0x7f050008;
-        public static int button_load=0x7f050009;
-        public static int button_send=0x7f050002;
-        public static int button_upload=0x7f05000a;
-        public static int edit_message=0x7f050001;
-        public static int hello_world=0x7f050006;
-        public static int title_activity_display_message=0x7f050005;
-        public static int title_activity_main=0x7f050004;
-        public static int title_activity_table_populate=0x7f05000e;
-        public static int title_activity_table_select=0x7f05000d;
+        public static final int action_search=0x7f050007;
+        public static final int action_settings=0x7f050003;
+        public static final int app_name=0x7f050000;
+        public static final int big_booties=0x7f05000c;
+        public static final int button_edit=0x7f05000b;
+        public static final int button_inventory=0x7f050008;
+        public static final int button_load=0x7f050009;
+        public static final int button_send=0x7f050002;
+        public static final int button_upload=0x7f05000a;
+        public static final int edit_message=0x7f050001;
+        public static final int hello_world=0x7f050006;
+        public static final int title_activity_display_message=0x7f050005;
+        public static final int title_activity_main=0x7f050004;
+        public static final int title_activity_table_populate=0x7f05000e;
+        public static final int title_activity_table_select=0x7f05000d;
     }
     public static final class style {
         /** 
@@ -75,10 +75,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f060001;
     }
 }
